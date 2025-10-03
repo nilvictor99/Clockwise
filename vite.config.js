0ae910209f06feea032d28e586d94b8a3b1ff107
victor <nilvictor99@gmail.com>
2025-10-03 13:23:36 -0500

    Remove build configuration and asset file naming from Vite config

--- vite.config.js
+++ vite.config.js
@@ -20,30 +20,4 @@ export default defineConfig({
         i18n(),
     ],
     base: '/',
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            output: {
-                manualChunks: undefined,
-                assetFileNames: (assetInfo) => {
-                    let extType = assetInfo.name.split('.').at(1);
-                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-                        extType = 'images';
-                    }
-                    return `assets/${extType}/[name]-[hash][extname]`;
-                },
-                chunkFileNames: 'assets/js/[name]-[hash].js',
-                entryFileNames: 'assets/js/[name]-[hash].js',
-            },
-        },
-    },
-    resolve: {
-        alias: {
-            '@': '/resources/js'
-        }
-    },
-    optimizeDeps: {
-        include: ['vue']
-    }
 });
