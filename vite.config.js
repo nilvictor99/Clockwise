ff623a10dc878c542858ac2b61ff4e0b8aebbe8e
victor <nilvictor99@gmail.com>
2025-10-03 14:47:01 -0500

    Refactor Vite config to set assets directory and enhance asset file naming

--- vite.config.js
+++ vite.config.js
@@ -28,19 +28,22 @@ export default defineConfig({
     build: {
         manifest: true,
         outDir: 'public/build',
-        assetsDir: '',
+        assetsDir: 'assets',
         rollupOptions: {
             output: {
                 manualChunks: undefined,
-                assetFileNames: '[name].[hash][extname]',
-                chunkFileNames: '[name].[hash].js',
-                entryFileNames: '[name].[hash].js',
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.').at(1);
+                    return `assets/${extType}/[name]-[hash][extname]`;
+                },
+                chunkFileNames: 'js/[name]-[hash].js',
+                entryFileNames: 'js/[name]-[hash].js',
             },
         },
     },
     server: {
         hmr: {
-            host: 'localhost',
-        },
-    },
+            host: 'localhost'
+        }
+    }
 });
